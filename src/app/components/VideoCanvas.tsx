cd9383b0db05ce180b61a1c1011b453527311f13
AndrewZamora <andrewmichaelzamora@gmail.com>
2024-01-02 17:50:59 +0900

    bug: fixed aspect ratio issue

---
+++ src/app/components/VideoCanvas.tsx
@@ -0,0 +1,84 @@
+'use client'
+import React, { useEffect, useRef } from 'react'
+// https://github.com/ml5js/ml5-library/issues/570
+const ml5 = require('ml5');
+type PoseNet = (arg0: HTMLVideoElement, arg1?: () => void) => Promise<any>;
+
+type PoseNetResponse = {
+  on?: (arg0: string, arg1: (result: any) => void) => void
+}
+const { poseNet }: { poseNet: PoseNet } = ml5;
+
+type Props = {
+  height: number,
+  width: number,
+}
+
+let navigatorIsReady = false;
+
+function VideoCanvas({ height, width }: Props) {
+  let canvas = useRef<HTMLCanvasElement>(null)
+  let video = useRef<HTMLVideoElement>(null);
+  let context: null | CanvasRenderingContext2D | undefined = null;
+  let constraints = { video: true }
+  let _poseNet: null | any = null;
+  let pose: null | any = null;
+  function poseNetLoaded() {
+    console.log("loaded")
+  }
+  function handlePose(poses: [object, object]) {
+    if (poses.length) {
+      pose = poses[0].pose;
+    }
+  }
+  function updateCanvas() {
+    if (video && video.current && canvas && canvas.current) {
+      context?.drawImage(video.current, 0, 0, canvas.current.width, canvas.current.height);
+      if (pose && context) {
+        context.strokeRect(pose.nose.x - 32, pose.nose.y - 32, 64, 64);
+        context.fillStyle = 'red';
+        context.strokeStyle = 'red';
+        context.stroke();
+      }
+    }
+    requestAnimationFrame(updateCanvas);
+  }
+  useEffect(() => {
+    return () => {
+      if (video && video.current) {
+        video.current.removeEventListener('play', updateCanvas);
+      }
+    }
+  }, [])
+  function handleMediaStream(mediaStream: MediaStream) {
+    if (video && video.current) {
+      if (video.current && "srcObject" in video.current) {
+        video.current.srcObject = mediaStream;
+      }
+      context = canvas.current?.getContext('2d');
+    }
+    if (video && video.current) {
+      video.current.addEventListener('play', updateCanvas);
+      _poseNet = poseNet(video.current, poseNetLoaded);
+      if (_poseNet) {
+        _poseNet.on('pose', handlePose);
+      }
+    }
+  }
+
+  if (typeof window !== 'undefined') {
+    if (!navigatorIsReady) {
+      navigatorIsReady = true
+      navigator.mediaDevices.getUserMedia(constraints).then((mediaStream: MediaStream) => handleMediaStream(mediaStream));
+    }
+  }
+  return (
+    <div>
+      <video ref={video} height={height} width={width} autoPlay></video>
+      <canvas ref={canvas} height={height} width={width}></canvas>
+    </div>
+
+  )
+}
+
+export default VideoCanvas
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import dynamic from 'next/dynamic'
-import Image from 'next/image'
 
-const Video = dynamic(()=> import('@/app/components/Video'), {ssr: false})
+const VideoCanvas = dynamic(()=> import('@/app/components/VideoCanvas'), {ssr: false})
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Video height={720} width={1280}  />
-    // </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <VideoCanvas height={540} width={720}  />
+    </main>
   )
 }
